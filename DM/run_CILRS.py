76b1a81fff7fee1a365bd0ab27554d2e83250229
UniAdaRepo <156202916+UniAdaRepo@users.noreply.github.com>
2024-01-15 10:35:25 +0800

    Update run_CILRS.py

--- DM/run_CILRS.py
+++ DM/run_CILRS.py
@@ -67,9 +67,9 @@ model.cuda()
 model.eval()  # this is for turn off dropout
 full_dataset = os.path.join(os.environ["COIL_DATASET_PATH"], 'CARLA100')
 titles = ['episode_00000_pedestrian', 'zip3,00715,white_car,raining', 'zip6_epi02431_black car',
-          'zip06,car,epi02472', 'zip3,00714,blue_car',
-          'zip14,05013,red_light', 'OK,zip14,epi04600,car']
-save_path = '/home/jzhang2297/gradnorm/results/CILRS_logger/DeepManeuver/'
+          'zip06,gray_car,epi02472', 'zip3,00714,blue_car',
+          'zip14,05013,red_light', 'zip14,epi04600,light_blue_car']
+save_path = './gradnorm/results/CILRS_logger/DeepManeuver/'
 noises = [15]
 iter=400
 for noise in noises:
